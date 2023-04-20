7349f8eabd52b3c52913c950598cc5b46ae75267
muzammil.peer <muzammil.peer@democrance.com>
2023-04-20 17:20:09 +0400

    fix

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
 //         .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "CoordinatorLibrary", url: "http://muzammilpeer.com:9090/scm/sw/coordinatorlibrary.git", .branch("master")),
+        .package(name: "CoordinatorLibrary", url: "https://github.com/iOS-App-Development/coordinatorlibrary.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
